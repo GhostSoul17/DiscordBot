4789a1681e8a450f5d8134168d750f7c8e52ea1e
Kyle Coger <kylecoger17@gmail.com>
2022-06-10 21:10:08 -0400

    Lauren said...

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if(message.content == "hey" || message.content == "yo"){
+    if(message.content.toLowerCase() == "hey" || message.content.toLowerCase() == "yo"){
         message.reply("I notice you...")
     }
 })
