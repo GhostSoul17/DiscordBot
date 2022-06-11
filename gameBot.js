ce5011cbec6115315db5910429215cb608bdee73
carlos <clsantiago@student.fullsail.edu>
2022-06-11 04:04:30 -0400

    fixed time

--- gameBot.js
+++ gameBot.js
@@ -47,7 +47,7 @@ class GameBot {
                 
             if(message.content.toLowerCase().includes("play")){
                 message.channel.send("What would you like to play? \nGames: 0, 1, 2").then(() => {
-                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 3000 })
+                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 120, errors: ["time"] })
                     .then(m => {
                         let choice = m.first().content.toLowerCase();
 
@@ -55,7 +55,7 @@ class GameBot {
                             case "0":
                                 message.channel.send("I am thinking of a number between 1 and 100.\nWhat number am I thinking of?")
                                 .then(() => {
-                                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 3000 })
+                                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 120, errors: ["time"] })
                                     .then(m => {
                                         let answer = Math.floor(Math.random * (1 - 100));
 
