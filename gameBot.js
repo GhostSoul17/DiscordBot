d9b1606f618fe378b78fdad5b39bbd7dfae252de
carlos <clsantiago@student.fullsail.edu>
2022-06-11 03:58:02 -0400

    adjusted game bot to reply and answer

--- gameBot.js
+++ gameBot.js
@@ -41,49 +41,55 @@ class GameBot {
 
     gameCall() {
         this.client.on("messageCreate", (message) => {
-            
+            const { member } = message;
+
             if (!message.content.toLowerCase().startsWith(this.starterG)) return;
                 
             if(message.content.toLowerCase().includes("play")){
-                message.reply("What would you like to play? \nGames: 0, 1, 2")
-                let choice = message.content.toLowerCase();
-                if(message.content.toLowerCase().includes("0")){
-                    choice = "0";
-                }
-                if(message.content.toLowerCase().includes("1")){
-                    choice = "1";
-                }
-                if(message.content.toLowerCase().includes("2")){
-                    choice = "2";
-                }
-            }
+                message.channel.send("What would you like to play? \nGames: 0, 1, 2").then(() => {
+                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 3000 })
+                    .then(m => {
+                        let choice = m.first().content.toLowerCase();
 
-            switch (choice) {
-                case "0":
-                    message.reply("I am thinking of a number between 1 and 100.\nWhat number am I thinking of?")
-                    let answer = Math.floor(Math.random * (1 - 100));
-                    if (message.content.toLowerCase() == answer) {
-                        message.reply(`Yes! I was totally thinking of ${answer}!`)
-                    }
-                    else {
-                        message.reply(`Womp womp, I was thinking of ${answer}. You lose.`)
-                    }
-                    break;
-                case "1":
-                    message.reply("What animal am I thinking of?")
-                    let animal = this.animalARRAY[Math.floor(Math.random * this.animalARRAY.length)];
-                    if(message.content.toLowerCase().includes(animal)){
-                        message.reply(`You sly dog! ${animal} died though, but you won!`);
-                    }else{
-                        message.reply(`${animal} decided to eat you...try again`);
-                    }
-                    break;
-                case "2":
-                    message.reply("Totally gonna add a game later, pick a different game");
-                    break;
-                default:
-                    message.reply("Nothing would make me happier");
-                    break;
+                        switch (choice) {
+                            case "0":
+                                message.channel.send("I am thinking of a number between 1 and 100.\nWhat number am I thinking of?")
+                                .then(() => {
+                                    message.channel.awaitMessages(m => m.author.id == member.id, { max: 1, time: 3000 })
+                                    .then(m => {
+                                        let answer = Math.floor(Math.random * (1 - 100));
+
+                                        if (m.content.toLowerCase() == answer) {
+                                            message.reply(`Yes! I was totally thinking of ${answer}!`)
+                                        }
+                                        else {
+                                            message.reply(`Womp womp, I was thinking of ${answer}. You lose.`)
+                                        }
+                                    });
+                                }).catch(collected => {
+                                    message.reply('Game timed out. Try again.');
+                                });
+                                break;
+                            case "1":
+                                message.reply("What animal am I thinking of?")
+                                let animal = this.animalARRAY[Math.floor(Math.random * this.animalARRAY.length)];
+                                if(message.content.toLowerCase().includes(animal)){
+                                    message.reply(`You sly dog! ${animal} died though, but you won!`);
+                                }else{
+                                    message.reply(`${animal} decided to eat you...try again`);
+                                }
+                                break;
+                            case "2":
+                                message.reply("Totally gonna add a game later, pick a different game");
+                                break;
+                            default:
+                                message.reply("Nothing would make me happier");
+                                break;
+                        }
+                    }).catch(collected => {
+                        message.reply('Game timed out. Try again.');
+                    });
+                })
             }
         })
     };
