2bed0e30af066a902f7bc8a388f1209c9e065a81
carlos <clsantiago@student.fullsail.edu>
2022-06-12 01:32:23 -0400

    added console logs

--- commands/baseCommands.js
+++ commands/baseCommands.js
@@ -2,10 +2,12 @@ import { client } from "../main.js";
 
 export default function messageCommand(callback, starter = []) {
     client.on("messageCreate", (message) => {
+        console.log(starter);
         if (starter.length > 0)
         {
             for (const prefix in starter)
             {
+                console.log(prefix);
                 if (!message.content.startsWith(prefix)) return;
             }
         }
