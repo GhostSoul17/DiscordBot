77f90fc15078a2d24142153a9cb220d2f0f35855
carlos <clsantiago@student.fullsail.edu>
2022-06-17 01:45:58 -0400

    changed code to run from python script

--- sarcasmCommands/sarcasmCommands.js
+++ sarcasmCommands/sarcasmCommands.js
@@ -19,11 +19,26 @@ class SarcasmCommands {
 
 function runSarcasmCheck(sarcasmPhrase) {
     let phrase = '';
-    let options = {
-        args: [sarcasmPhrase]
-    };
+    let pythonScript = `import pandas as pd
+    import numpy as np
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.model_selection import train_test_split
+    from sklearn.naive_bayes import BernoulliNB
+    
+    data = pd.read_json("Sarcasm.json", lines=True)
+    data = data[["headline", "is_sarcastic"]]
+    x = np.array(data["headline"])
+    y = np.array(data["is_sarcastic"])
+    cv = CountVectorizer()
+    X = cv.fit_transform(x) # Fit the Data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    model = BernoulliNB()
+    model.fit(X_train, y_train)
+    data = cv.transform(${sarcasmPhrase}).toarray()
+    output = model.predict(data)
+    print(output)`;
 
-    PythonShell.run('sarcasmScript.py', options, function (error, results) {
+    PythonShell.runScript(pythonScript, null, function (error, results) {
         if (error) throw error;
 
         phrase = results[0];
